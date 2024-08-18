678ff56c4dfbb1bbf7b46af5f04521e87db1c34d
margarita <margarita.que@mail.ru>
2024-08-19 01:12:29 +0400

    3. Арифметические операции. [#185]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Margarita Korol");
+        System.out.println("09.06.20000");
+    }
+
+}
