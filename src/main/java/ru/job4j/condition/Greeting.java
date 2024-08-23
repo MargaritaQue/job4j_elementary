2292a8474e8337998df32154f7fd0ac6ab7d4f65
margarita <margarita.que@mail.ru>
2024-08-23 15:26:23 +0400

    9.1. String [#237407 #510261]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class Greeting {
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
+
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
